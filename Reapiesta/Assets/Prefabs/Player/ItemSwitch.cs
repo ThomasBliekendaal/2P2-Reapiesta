531550eebe4f0299dee7d6c5fc9ff7ff2b3dc729
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2018-11-27 13:42:25 +0100

    PlayerPrefab
    Will it work? Who knows.
    Also changed some physics for the skateboard a little bit.

---
+++ Reapiesta/Assets/Prefabs/Player/ItemSwitch.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.PostProcessing;
+
+public class ItemSwitch : MonoBehaviour
+{
+
+    public int curItem = 0;
+    [SerializeField] Text ui;
+    [SerializeField] ScytheThrow special;
+    [SerializeField] int specialDisable = 0;
+    PostProcessingBehaviour pp;
+
+    void Start()
+    {
+        pp = Camera.main.GetComponent<PostProcessingBehaviour>();
+    }
+
+    void Update()
+    {
+        Scroll();
+        SetActives();
+        ActivateSpecial();
+    }
+
+    void SetActives()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (i == curItem)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void Scroll()
+    {
+        curItem += (int)Input.mouseScrollDelta.y;
+        if (curItem > transform.childCount - 1)
+        {
+            curItem = 0;
+        }
+        if (curItem < 0)
+        {
+            curItem = transform.childCount - 1;
+        }
+        ui.text = transform.GetChild(curItem).name;
+        Time.timeScale = 1;
+        pp.profile.motionBlur.enabled = false;
+    }
+
+    void ActivateSpecial()
+    {
+        if (curItem == specialDisable && Input.GetButtonDown("Throw"))
+        {
+            // special.SetActive(false);
+            curItem++;
+        }
+		 if (curItem == specialDisable && special.curState != ScytheThrow.State.Disabled)
+        {
+            Scroll();
+        }
+    }
+}
---
+++ Reapiesta/Assets/Prefabs/Player/Shoot.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class Shoot : MonoBehaviour
+{
+
+    public float range = 100;
+    PlayerController plyr;
+    Camera cam;
+    PostProcessingBehaviour pp;
+    [SerializeField] float slowMotionSpeed = 0.3f;
+    void Start()
+    {
+        plyr = FindObjectOfType<PlayerController>();
+        pp = Camera.main.GetComponent<PostProcessingBehaviour>();
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        Controll();
+    }
+
+    void Controll()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            ShootNow();
+            Time.timeScale = 1;
+            pp.profile.motionBlur.enabled = false;
+        }
+        if (Input.GetAxis("Fire1") == 0)
+        {
+            //cam.fieldOfView = Mathf.MoveTowards(cam.fieldOfView, 60, Time.deltaTime * 150);
+            Time.timeScale = 1;
+            pp.profile.motionBlur.enabled = false;
+        }
+        else
+        {
+            if (plyr.cc.isGrounded == false && plyr.curState != PlayerController.State.SkateBoard)
+            {
+                Time.timeScale = slowMotionSpeed;
+                pp.profile.motionBlur.enabled = true;
+                // cam.fieldOfView = 40;
+            }
+            else if (plyr.curState == PlayerController.State.SkateBoard && plyr.grounded == false && plyr.cc.isGrounded == false)
+            {
+                Time.timeScale = slowMotionSpeed;
+                pp.profile.motionBlur.enabled = true;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pp.profile.motionBlur.enabled = false;
+            }
+        }
+
+    }
+
+    void ShootNow()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        if (Physics.Raycast(ray, out hit, range))
+        {
+            if (hit.transform.tag == "Enemy")
+            {
+                cam.GetComponent<Cam>().StartShake(0.2f, 0.5f);
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+}
