d21dd858dfca7dcdc2b4b7d0851cf6758be652ff
PimHuisman <33859395+PimHuisman@users.noreply.github.com>
2018-12-10 09:59:31 +0100

    weaponscript

--- Reapiesta/Assets/Scripts/Combat/MainWeapon.cs
+++ Reapiesta/Assets/Scripts/Combat/MainWeapon.cs
@@ -5,28 +5,35 @@ using UnityEngine.UI;
 
 public class MainWeapon : MonoBehaviour
 {
-	[Header("Ammo")]
+    [Header("Ammo")]
     [SerializeField] int Clipsize;
     [SerializeField] Text ammoAmountText;
     int currentAmmoAmount;
     [SerializeField] float forceAmount;
     [SerializeField] Transform bullet;
-	[Header("Partical")]
-	[SerializeField] GameObject shootPartical;
+    [SerializeField] Transform barrelEnd;
+    [Header("Partical")]
+    [SerializeField] GameObject shootPartical;
+    Transform camPos;
+    RaycastHit hit;
+    [SerializeField] float rayLenght;
     void Start()
     {
+        camPos = Camera.main.transform;
         currentAmmoAmount = Clipsize;
     }
 
     void Update()
     {
         Shoot();
+        Reload();
     }
 
     void Reload()
     {
         if (Input.GetButtonDown("Reload"))
         {
+            print("Reload");
             // set ammoAmount to max Ammo
             currentAmmoAmount = Clipsize;
             // trigger the UIFunction() for ammoUI
@@ -39,19 +46,43 @@ public class MainWeapon : MonoBehaviour
         //	when click shoot
         if (Input.GetButtonDown("Attack"))
         {
-            //  subtract bullet
-            currentAmmoAmount--;
-            //	instantiate bullet
-            Transform newBullet = Instantiate(bullet);
-            //	add force to the bullet
-            newBullet.GetComponent<Rigidbody>().AddForce(newBullet.forward);
-            UIFunction();
+            if (currentAmmoAmount > 0)
+            {
+                if (Physics.Raycast(camPos.position, camPos.forward, out hit, rayLenght))
+                {
+                    //  subtract bullet
+                    currentAmmoAmount--;
+                    //	instantiate bullet
+                    Transform newBullet = Instantiate(bullet, barrelEnd.position, barrelEnd.rotation);
+                    Rigidbody addRigid = newBullet.GetComponent<Rigidbody>();
+                    //	add force to the bullet
+                    addRigid.velocity = (hit.point - transform.position).normalized * forceAmount;
+                    addRigid.rotation = Quaternion.LookRotation(addRigid.velocity);
+                    UIFunction();
+                }
+                else
+                {
+                    //  subtract bullet
+                    currentAmmoAmount--;
+                    //	instantiate bullet
+                    Transform newBullet = Instantiate(bullet, barrelEnd.position, barrelEnd.rotation);
+                    Rigidbody addRigid = newBullet.GetComponent<Rigidbody>();
+                    //	add force to the bullet
+                    addRigid.velocity = (transform.forward - transform.position).normalized * forceAmount;
+                    addRigid.rotation = Quaternion.LookRotation(addRigid.velocity);
+                    UIFunction();
+                }
+            }
+            if (currentAmmoAmount <= 0)
+            {
+                currentAmmoAmount = 0;
+            }
         }
     }
 
     void UIFunction()
     {
         // subtract bullet ammount
-        ammoAmountText.text = currentAmmoAmount.ToString()+"/"+"Ammo";
+        ammoAmountText.text = currentAmmoAmount + "/" + "Ammo".ToString();
     }
 }
