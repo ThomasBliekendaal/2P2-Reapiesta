9c9302d6499d96f0a42ec3a19a8957e3c4e28f46
CaspervanderKaaij <33858977+CaspervanderKaaij@users.noreply.github.com>
2018-12-11 10:08:41 +0100

    Better UI
    I did not steal it from Persona 5.
    They stole it from me obviously.
    jk, I added some little icons and styled it up.

---
+++ Reapiesta/Assets/Scripts/Menu/AudioVisualizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVisualizer : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    public float[] samples = new float[7];
+    public List<RectTransform> toScale;
+
+    void Start()
+    {
+		toScale.Clear();
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			toScale.Add(transform.GetChild(i).GetComponent<RectTransform>());
+		}
+    }
+
+    void Update()
+    {
+        GetAudioInfo();
+        SetScale();
+    }
+
+    void GetAudioInfo()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void SetScale()
+    {
+        for (int i = 0; i < toScale.Count; i++)
+        {
+            toScale[i].localScale = new Vector3(toScale[i].localScale.x, samples[i] * 20, toScale[i].localScale.z);
+        }
+    }
+}
